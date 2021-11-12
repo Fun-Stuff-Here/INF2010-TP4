4b6937b23a29c814cf9f370feb9cda45469f98ba
Nicolas Depelteau <71102008+Depdx@users.noreply.github.com>
2021-11-12 13:40:32 -0500

    interview

--- src/Interview.java
+++ src/Interview.java
@@ -3,12 +3,25 @@ import java.util.PriorityQueue;
 
 public class Interview {
 
+        //Time complexite est de O(nlogn)
         public int lastBox(int[] boxes){
 
             // Ne pas modifier la ligne suivante
             PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
 
-            return -1;
+            for (int box:boxes) heap.add(box); //O(nlogn)
+            return lastBox(heap);//O(nlogn)
+        }
+
+        private int lastBox(PriorityQueue<Integer> heap){
+            if(heap.size() == 0) return 0;
+            if(heap.size() == 1) return heap.poll();
+
+            //Collision
+            Integer box2 = heap.poll();
+            Integer box1 = heap.poll();
+            if(!box1.equals(box2)) heap.add(box2-box1);//O(logn)
+            return lastBox(heap);
         }
 }
 
